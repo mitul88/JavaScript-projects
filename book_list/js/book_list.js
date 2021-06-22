aeda1d92be7bb5b140cf6eb5d32b4af3fcafd962
shahed <shahed.rahman88@gmail.com>
2021-06-23 00:43:57 +0600

    add book_list project

---
+++ book_list/js/book_list.js
@@ -0,0 +1,89 @@
+// Get the UI element
+
+let form = document.querySelector('#book-form');
+let booklist = document.querySelector('#book-list');
+
+// Book Class
+
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+// UI Class
+
+class UI {
+
+    static addToBooklist(book) {
+        let list = document.querySelector('#book-list');
+        let row = document.createElement('tr');
+
+        row.innerHTML =
+            `<td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class="delete">X</a></td>`
+
+        list.appendChild(row);
+    }
+
+    static clearFields() {
+        document.querySelector('#title').value = "";
+        document.querySelector('#author').value = "";
+        document.querySelector('#isbn').value = "";
+    }
+
+    static showAlert(message, className) {
+        let div = document.createElement('div');
+        div.className = `alert ${className}`;
+        div.appendChild(document.createTextNode(message));
+        let container = document.querySelector('.container');
+        let form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+
+        setTimeout(() => {
+            document.querySelector('.alert').remove();
+        }, 3000);
+    }
+
+    static deleteFromBook(target) {
+        if (target.hasAttribute('href')) {
+            target.parentElement.parentElement.remove();
+            UI.showAlert('Book Removed !', 'success');
+        }
+    }
+}
+
+// Add event listener
+form.addEventListener('submit', newBook);
+booklist.addEventListener('click', removeBook);
+
+// Define function
+function newBook(e) {
+    let title = document.querySelector('#title').value,
+        author = document.querySelector('#author').value,
+        isbn = document.querySelector('#isbn').value;
+
+
+    if (title === '' || author === '' || isbn === '') {
+        UI.showAlert("Please fill up all the fields !", "error")
+    } else {
+        let book = new Book(title, author, isbn);
+
+        UI.addToBooklist(book);
+        UI.showAlert("Your Book Added !", "success")
+
+        UI.clearFields();
+    }
+
+    e.preventDefault();
+}
+
+function removeBook(e) {
+
+    UI.deleteFromBook(e.target);
+    e.preventDefault();
+}
